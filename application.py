fa15dd3f511ff677f154049a7303fe5bb8eafe33
Hongyang Bai <hongyangbai@dyn-160-39-134-173.dyn.columbia.edu>
2016-05-08 15:31:58 -0400

    add many

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ from flask_socketio import SocketIO, emit, join_room, leave_room, \
 import json 
 import requests
 from elasticsearch import Elasticsearch
+import sqlalchemy
 
 monkey.patch_all()
 application = Flask(__name__)
@@ -13,6 +14,29 @@ application.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(application, async_mode='gevent')
 
 disp_process_pref = "===[Process]===: "
+
+#####################################
+#
+# For hanlding PostGreSQL
+#
+#####################################
+conn_str = 'postgresql://movie:nerds@52.87.217.221:5432/movie'
+
+engine = sqlalchemy.create_engine(conn_str)
+
+@application.before_request
+def before_request():
+    try:
+        g.conn = engine.connect()
+    except:
+        traceback.print_exc()
+        g.conn = None
+
+
+@application.teardown_request
+def teardown_request(_):
+    if g.conn is not None:
+        g.conn.close()
 
 
 # Render home page
@@ -26,20 +50,53 @@ def index_():
     return render_template('index.html')
 
 
-@application.route('/signin')
+@application.route('/signin', methods = ['GET', 'POST'])
 def signin():
+
+
+    error = None
+    if request.method == 'POST':
+        print request.form.get('user-mail')
+        print request.form.get('user-password')
+        username = request.form.get('user-mail')
+        password = request.form.get('user-password')
+        '''cur = g.conn.execute('''SELECT * FROM users WHERE username = %s AND password = %s''', (username, password))
+        user = cur.fetchone()
+        if user is None:
+            error = 'Invalid username or password.'
+        else:
+            session['username'] = username
+            flash('You are now logged in as <b>{}</b>.'.format(username))'''
+            return redirect(url_for('index'))
 
     return render_template('signin.html')
 
+@application.route('/profile')
+def profile():
 
-@application.route('/signin', methods = ['POST'])
+    return render_template('profile.html')
+
+'''@application.route('/signin', methods = ['GET', 'POST'])
 def getSignin():
     print request.form.get('user-mail')
     print request.form.get('user-password')
 
 
+    error = None
+    if request.method == 'POST':
+        username = request.form.get('user-mail')
+        password = request.form.get('user-password')
+        cur = g.conn.execute('''SELECT * FROM users WHERE username = %s AND password = %s''', (username, password))
+        user = cur.fetchone()
+        if user is None:
+            error = 'Invalid username or password.'
+        else:
+            session['username'] = username
+            flash('You are now logged in as <b>{}</b>.'.format(username))
+            return redirect(url_for('home'))
 
-    return render_template('index.html')
+
+    return render_template('index.html')'''
 
 
 @application.route('/signup')
